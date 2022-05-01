33317b04fa17b50a84ae05322fcc36088357ae1a
thfurkanabay <furkanabay61@gmail.com>
2022-05-01 16:33:39 +0300

    Add old project files
    Add old project files

---
+++ HexFall_Demo/Assets/Scripts/Nodes.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodes : MonoBehaviour
+{
+
+public Vector3 node;
+
+
+public void Start(){
+
+
+}
+
+
+
+}
---
+++ HexFall_Demo/Assets/Scripts/THF_UI_System.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace THF.UI
+{
+    public class THF_UI_System : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+
+        #region Main Methods
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        #endregion
+
+        #region Helper Methods
+        
+        #endregion
+    }
+}
